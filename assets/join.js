9fd586f35663aaeff2db10a443878819327512f1
Raymond Thurman <raymondthurman5@gmail.com>
2019-05-19 19:15:03 -0400

    join.js refactored into component, needs tested

--- assets/join.js
+++ assets/join.js
@@ -1,32 +1,44 @@
-// Name and Password from the register-form
-const name = document.getElementById('userName');
-const email = document.getElementById('userEmail');
-const pw = document.getElementById('userPw');
-const profile = document.getElementById('userProfile');
+class Form {
+    constructor(element) {
+        this.element = element;
+        this.name = element.getElementById('userName');
+        this.email = element.getElementById('userEmail');
+        this.pw = element.getElementById('userPw');
+        this.profile = element.getElementById('userProfile');
+        this.joinButton = element.querySelector('.join-now');
+        this.loginButton = element.querySelector('.log-in');
+        this.joinButton.addEventListener('click', () => this.store());
+        this.loginButton.addEventListener('click', () => this.check());
+    }
 
-// storing input from register-form
-function store() {
-    localStorage.setItem('name', name.value);
-    localStorage.setItem('email', email.value);
-    localStorage.setItem('pw', pw.value);
-    localStorage.setItem('profile', profile.value);
-}
+    // storing input from register-form
+    store() {
+        localStorage.setItem('name', this.name.value);
+        localStorage.setItem('email', this.email.value);
+        localStorage.setItem('pw', this.pw.value);
+        localStorage.setItem('profile', this.profile.value);
+    }
 
-// check if stored data from register-form is equal to entered data in the   login-form
-function check() {
+    // check if stored data from register-form is equal to entered data in the   login-form
+    check() {
 
-    // stored data from the register-form
-    const storedName = localStorage.getItem('name');
-    const storedPw = localStorage.getItem('pw');
+        // stored data from the register-form
+        const storedName = localStorage.getItem('name');
+        const storedPw = localStorage.getItem('pw');
 
-    // entered data from the login-form
-    const name = document.getElementById('userName').value;
-    const pw = document.getElementById('userPw').value;
+        // entered data from the login-form
+        const name = document.getElementById('userName').value;
+        const pw = document.getElementById('userPw').value;
 
-    // check if stored data from register-form is equal to data from login form
-    if (userName.value !== storedName || userPw.value !== storedPw) {
-        alert('ERROR');
-    } else {
-        alert('You are loged in.');
+        // check if stored data from register-form is equal to data from login form
+        if (userName.value !== storedName || userPw.value !== storedPw) {
+            alert('ERROR');
+        } else {
+            alert('You are loged in.');
+        }
     }
 }
+
+
+
+let forms = document.querySelector('.form-content').forEach(form => new Form(form));
