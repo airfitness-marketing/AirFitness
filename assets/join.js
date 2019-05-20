f6ddf15c072cc7eb80f927dab3a2afa7a136c22a
Raymond Thurman <raymondthurman5@gmail.com>
2019-05-19 20:03:39 -0400

    registration and login working correctly

--- assets/join.js
+++ assets/join.js
@@ -7,8 +7,7 @@ class Form {
         this.profile = element.querySelector('#userProfile');
         this.joinButton = element.querySelector('.join-now');
         this.loginButton = element.querySelector('.log-in');
-        // this.joinButton.addEventListener('click', () => this.store());
-        this.loginButton.addEventListener('click', () => this.check());
+        this.joinButton.addEventListener('click', () => this.store());
     }
 
     // storing input from register-form
@@ -17,25 +16,6 @@ class Form {
         localStorage.setItem('email', this.email.value);
         localStorage.setItem('pw', this.pw.value);
         localStorage.setItem('profile', this.profile.value);
-    }
-
-    // check if stored data from register-form is equal to entered data in the   login-form
-    check() {
-
-        // stored data from the register-form
-        const storedName = localStorage.getItem('name');
-        const storedPw = localStorage.getItem('pw');
-
-        // entered data from the login-form
-        this.name = document.querySelector('#userName');
-        this.pw = document.querySelector('#userPw');
-
-        // check if stored data from register-form is equal to data from login form
-        if (this.name.value !== storedName || this.userPw.value !== storedPw) {
-            alert('ERROR');
-        } else {
-            alert('You are loged in.');
-        }
     }
 }
 
